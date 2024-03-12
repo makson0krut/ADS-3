043f1ac0d82eaf923e00dd0ab8c8d429fc0d9560
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-12 12:21:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@ int cbinsearch(int *arr, int size, int value) {
   int right = size - 1;
   int firstFlag = - 1;
   int lastFlag = -1;
-  while (lefy <= right) {
+  while (left <= right) {
     int mid = left + (right - left) / 2;
     if (arr[mid] == value) {
       firstFlag = mid;
@@ -21,7 +21,7 @@ int cbinsearch(int *arr, int size, int value) {
   while (left <= right) {
     int mid = left + (right - left) / 2;
       if (arr[mid] == value) {
-          lastOccurrence = mid;
+          lastFlag = mid;
           left = mid + 1;
       } else if (arr[mid] < value) {
           left = mid + 1;
@@ -29,7 +29,7 @@ int cbinsearch(int *arr, int size, int value) {
           right = mid - 1;
         }
     }
-    if (firstOccurrence != -1 && lastOccurrence != -1) {
+    if (firstFlag != -1 && lastFlag != -1) {
         return lastOccurrence - firstOccurrence + 1;
     } else {
         return 0;
