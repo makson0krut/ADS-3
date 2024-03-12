70dbdc211d6cf2a9f7a1efd6855bf92981a95046
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-12 12:09:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  bool isDigit = false;
+  int flag = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == target) {
+      flag += 1;
+      isDigit = true;
+    } else {
+      return 0;
+    }
+  }
 }
