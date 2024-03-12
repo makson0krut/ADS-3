d9eea1c738b8cc0a02e83f5028a91c569b46332d
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-12 12:18:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,37 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  bool isDigit = false;
-  int flag = 0;
-  for (int i = 0; i < size; i++) {
-    if (arr[i] == value) {
-      flag += 1;
-      isDigit = true;
-      return flag;
+  int left = 0;
+  int right = size - 1;
+  int firstFlag = - 1;
+  int lastFlag = -1;
+  while (lefy <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == value) {
+      firstFlag = mid;
+      right = mid - 1;
+    } else if (arr[mid] < value) {
+        left = mid + 1;
     } else {
-      return 0;
+        right = mid - 1;
     }
   }
+  left = 0;
+  right = size - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+      if (arr[mid] == value) {
+          lastOccurrence = mid;
+          left = mid + 1;
+      } else if (arr[mid] < value) {
+          left = mid + 1;
+      } else {
+          right = mid - 1;
+        }
+    }
+    if (firstOccurrence != -1 && lastOccurrence != -1) {
+        return lastOccurrence - firstOccurrence + 1;
+    } else {
+        return 0;
+    }
 }
