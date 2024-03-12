88cc6c411704f1937d109bcbb93dfec5bbe0c83d
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-12 12:12:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,9 +4,10 @@ int cbinsearch(int *arr, int size, int value) {
   bool isDigit = false;
   int flag = 0;
   for (int i = 0; i < size; i++) {
-    if (arr[i] == target) {
+    if (arr[i] == value) {
       flag += 1;
       isDigit = true;
+      return flag;
     } else {
       return 0;
     }
